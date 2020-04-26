8d4a91717144ca5427f1429c7d3552a189c7a900
Andrew Su <avsu@uci.edu>
2020-04-26 03:26:18 -0700

    Optimized Search Query - Again - Pre Task 3

--- src/main/java/MovieServlet.java
+++ src/main/java/MovieServlet.java
@@ -31,34 +31,30 @@ public class MovieServlet extends HttpServlet {
         return "select movies.id, movies.title, movies.year, movies.director, group_concat(distinct genres.name separator ', ') as threeGenres, substring_index(group_concat(stars.name separator ','), ',', 3) as threeStars, substring_index(group_concat(stars.id separator ','), ',', 3) as threeStarIds, movies.rating from (select movies.id, movies.title, movies.year, movies.director, ratings.rating from movies left join ratings on (movies.id = ratings.movieId) order by ratings.rating desc limit 20) as movies left join stars_in_movies on (movies.id = stars_in_movies.movieId) left join stars on (stars.id = stars_in_movies.starId) left join genres_in_movies on (movies.id = genres_in_movies.movieId) left join genres on (genres.id = genres_in_movies.genreId) group by movies.id order by movies.rating desc;";
     }
     String constructSearchQuery(String starOfMovie, String titleOfMovie, String yearOfRelease, String directorOfMovie){
-        String searchQuery;
         // Return a default search if no fields were filled
         if (starOfMovie == null && titleOfMovie == null && yearOfRelease == null && directorOfMovie == null){
             return defaultMovieQuery();
         }
 
         // Return a custom search if fields were filled
-        searchQuery = "select movies.id, movies.title, movies.year, movies.director, group_concat(distinct genres.name separator ', ') as threeGenres, substring_index(group_concat(stars.name separator ','), ',', 3) as threeStars, substring_index(group_concat(stars.id separator ','), ',', 3) as threeStarIds, ratings.rating from (select movies.id, movies.title, movies.year, movies.director from movies left join stars_in_movies on (movies.id = stars_in_movies.movieId) left join stars on (stars.id = stars_in_movies.starId) ";
-        if (starOfMovie != null){
-            searchQuery += String.format("where stars.name like '%%%s%%'",starOfMovie);
-        }
-        searchQuery += ") as movies left join stars_in_movies on (movies.id = stars_in_movies.movieId) left join stars on (stars.id = stars_in_movies.starId) left join genres_in_movies on (movies.id = genres_in_movies.movieId) left join genres on (genres.id = genres_in_movies.genreId) left join ratings on (movies.id = ratings.movieId) ";
-        if (titleOfMovie!= null || yearOfRelease != null || directorOfMovie!= null){
-            searchQuery += "where ";
-            if (titleOfMovie!= null)
-                searchQuery += String.format("movies.title like '%%%s%%' AND ",titleOfMovie);
-            if (yearOfRelease!= null)
-                searchQuery += String.format("movies.year = %s AND ",yearOfRelease);
-            if (directorOfMovie!= null)
-                searchQuery += String.format("movies.director like '%%%s%%' AND ", directorOfMovie);
-            //Remove "AND " at the end.
-            searchQuery = searchQuery.substring(0,searchQuery.length()-4);
-        }
-        searchQuery += "group by movies.id order by ratings.rating desc limit 20;";
+        String searchQuery = "select movies.id, movies.title, movies.year, movies.director, group_concat(distinct genres.name separator ', ') as threeGenres, substring_index(group_concat(stars.name separator ','), ',', 3) as threeStars, substring_index(group_concat(stars.id separator ','), ',', 3) as threeStarIds, ratings.rating from (select movies.id, movies.title, movies.year, movies.director from movies left join stars_in_movies on (movies.id = stars_in_movies.movieId) left join stars on (stars.id = stars_in_movies.starId) where ";
+
+        if (starOfMovie!= null)
+            searchQuery += String.format("stars.name like '%%%s%%' AND ",starOfMovie);
+        if (titleOfMovie!= null)
+            searchQuery += String.format("movies.title like '%%%s%%' AND ",titleOfMovie);
+        if (yearOfRelease!= null)
+            searchQuery += String.format("movies.year = %s AND ",yearOfRelease);
+        if (directorOfMovie!= null)
+            searchQuery += String.format("movies.director like '%%%s%%' AND ", directorOfMovie);
+        //Remove "AND " at the end.
+        searchQuery = searchQuery.substring(0,searchQuery.length()-4);
+
+        searchQuery += "group by movies.id limit 20) as movies left join stars_in_movies on (movies.id = stars_in_movies.movieId) left join stars on (stars.id = stars_in_movies.starId) left join genres_in_movies on (movies.id = genres_in_movies.movieId) left join genres on (genres.id = genres_in_movies.genreId) left join ratings on (movies.id = ratings.movieId) group by movies.id order by ratings.rating desc limit 20;";
         return searchQuery;
     }
     String constructBrowseQuery(String byCategory, String givenCategory){
-        String browseQuery = "select movies.id, movies.title, movies.year, movies.director, group_concat(distinct genres.name separator ', ') as threeGenres, substring_index(group_concat(stars.name separator ','), ',', 3) as threeStars, substring_index(group_concat(stars.id separator ','), ',', 3) as threeStarIds, ratings.rating from (select movies.id, movies.title, movies.year, movies.director from movies ";
+        String browseQuery = "select movies.id, movies.title, movies.year, movies.director, group_concat(distinct genres.name order by genres.name asc separator ', ') as threeGenres, substring_index(group_concat(stars.name separator ','), ',', 3) as threeStars, substring_index(group_concat(stars.id separator ','), ',', 3) as threeStarIds, ratings.rating from (select movies.id, movies.title, movies.year, movies.director from movies ";
 
         if (byCategory.equals("title")) {
             browseQuery += "left join ratings on (movies.id = ratings.movieId) ";
